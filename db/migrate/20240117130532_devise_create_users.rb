7ca21662fcb7d687d64c5e30653222e02b232480
Thai Dang <thai.hulk@gmail.com>
2024-01-17 20:29:43 +0700

    rubocop

--- db/migrate/20240117130532_devise_create_users.rb
+++ db/migrate/20240117130532_devise_create_users.rb
@@ -4,9 +4,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :name,               null: false     
+      t.string :name,               null: false
       t.string :email,              null: false
-      t.string :encrypted_password, null: false, default: ""
+      t.string :encrypted_password, null: false, default: ''
 
       ## Recoverable
       # t.string   :reset_password_token
@@ -33,11 +33,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
+    add_index :users, :email, unique: true
     # add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
